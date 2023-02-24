5d445e1b6983d97000a5b92f3c9997f1897a0560
Dapo <Dapo.owolabi@outlook.com>
2023-02-24 14:52:26 -0500

    currency

--- stock_data.py
+++ stock_data.py
@@ -1,8 +1,32 @@
 import requests
+from datetime import datetime
+import time
 
-url = "https://query1.finance.yahoo.com/v7/finance/download/AAPL?period1=345427200&period2=1677196800&interval=1d&events=history&includeAdjustedClose=true"
+from_date = input('Enter Start Date in yyyy/mm/dd format: ')
+from_datetime = datetime.strptime(from_date,'%Y/%m/%d')
+from_epoch = int( time.mktime(from_datetime.timetuple()) )
+
+to_date = input('Enter End Date in yyyy/mm/dd format: ')
+to_datetime = datetime.strptime(to_date,'%Y/%m/%d') 
+to_epoch = int( time.mktime(to_datetime.timetuple()) )
+
+ticker = input('What stock data do you want? ')
 
 
-content = requests.get(url).content
+url = f"https://query1.finance.yahoo.com/v7/finance/download/{ticker}?period1={from_epoch}&period2={to_epoch}&interval=1d&events=history&includeAdjustedClose=true"
 
-print(content)
+headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36"}
+
+content = requests.get(url, headers=headers).content
+
+#content = str(content)
+#print(content)
+
+def stock_to_csv(content, ticker):
+    filename = f"{ticker}.csv"
+
+    with open(filename,'wb') as file:
+        file.write(content)
+
+
+stock_to_csv(content,ticker)
