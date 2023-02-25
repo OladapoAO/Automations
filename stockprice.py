6a7a9de12701ba34330b7692d225ec703fc6afa9
Dapo <Dapo.owolabi@outlook.com>
2023-02-25 17:44:01 -0500

    project_1

--- stockprice.py
+++ stockprice.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
@@ -16,7 +15,7 @@ def get_driver():
     options.add_argument("disable-blink-features=AutomationControlled")
 
     driver = webdriver.Chrome(options=options)
-    driver.get("http://automated.pythonanywhere.com")
+    driver.get("https://zse.hr/en/indeks-366/365?isin=HRZB00ICBEX6")
     return driver
 
 def clean_text(text):
@@ -24,20 +23,23 @@ def clean_text(text):
     output = text.split(": ")
     return float(output[1])
 
-def write_file(text):
-    """ Write input text into a text file"""
-    filename = f"{datetime.now().strftime('%Y-%m-%d.%H-%M-%S')}.txt"
-    with open(filename,'w') as file:
-        file.write(text)
-
 def main():
     driver = get_driver()
-    while True:
-        time.sleep(2)
-        element = driver.find_element(By.XPATH,"/html/body/div/div/h1[2]")
-        text = str(clean_text(element.text))
-        write_file(text)
+    time.sleep(3)
+    element = driver.find_element(By.XPATH,'//*[@id="app_indeks"]/section[1]/div/div/div[2]/span[2]')
+    element = element.text
+    element = element[0:5]
+    stock_percentage = float(element)
 
-  
+    return (stock_percentage, type(stock_percentage))
 
-print(main())
+stock_percentage = main()
+stock_percentage = stock_percentage[0]
+
+def send_email(stock):
+    if stock < -0.10 :
+        print(f'send email - Stock CROBEX is less than 0.10% @ {stock}')
+    else:
+        print(f'Stock CROBEX is at {stock}')
+
+send_email(stock_percentage)
