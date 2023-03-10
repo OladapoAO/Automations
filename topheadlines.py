a4d2278a578148382d2b5635a9623e7d66a754df
Dapo <Dapo.owolabi@outlook.com>
2023-03-10 15:29:19 -0500

    Updating News Api & Weather Api commencment

--- topheadlines.py
+++ topheadlines.py
@@ -12,5 +12,5 @@ def get_news(country, api_key=newsapi_key):
     
     return results
 
-macbooknews = get_news(topic='macbook', from_date='2023-02-10', to_date='2023-02-18')
+macbooknews = get_news(country='us')
 print(macbooknews[1])
